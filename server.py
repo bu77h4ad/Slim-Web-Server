b821701de997b8826a992d41edfa44ba6919bed0
bu77h4ad <31614065+bu77h4ad@users.noreply.github.com>
2018-06-15 17:11:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,49 @@
+import time
 import socket
+
 html = """
 <html>
     <head> <title>ESP8266 Pins</title> </head>
-    <body> <h1>ESP8266 Pins</h1>
-        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+    <body> <h1>ESP32</h1>
+        <p style="width: 100px;">%s</p>
     </body>
 </html>
 """
-addr = socket.getaddrinfo('192.168.4.1', 80)[0][-1]
+addr = socket.getaddrinfo('192.168.1.194', 80)[0][-1]
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 serv.bind(addr)
-serv.listen(10)
+serv.listen(0)
 
 print('listening on', addr)
 
 while True:
-    client_sock, client_addr = serv.accept()
-    client_sock_file = client_sock.makefile('rwb', 0)
-    line = client_sock_file.readline()
-    print ("CLIENT ADDRESS : ",client_addr)
-    while True:
+    try:
+        client_sock, client_addr = serv.accept()    
+        client_sock_file = client_sock.makefile('rwb', 0)
         line = client_sock_file.readline()
-        if not line or line == b'\r\n':
-            break
-    client_sock.send("HTTP/1.0 200 OK\r\n")    
-    client_sock.send("Content-Type: text/html\r\n")
-    client_sock.send("\r\n")
-    client_sock.send(html)
-    client_sock.close()
+        print ("CLIENT ADDRESS : ",client_addr) 
+        while True:
+            line = client_sock_file.readline()
+            print ("Client Resive ->", line)
+            if line == b'' or line == b'\r\n':
+                break
+        client_sock.send("HTTP/1.1 200 OK\r\n")    
+        client_sock.send("Server: MicroPython-WebServer\r\n")
+        client_sock.send("Content-Type: text/html; charset=UTF-8\r\n")
+        client_sock.send("\r\n")
+        f = open('cover.txt')
+        cover = f.read()
+        f.close()
+    
+        client_sock.send(html % (cover) )
+        client_sock.close()        
+    
+    except KeyboardInterrupt:
+        break
+    
+    except :
+        print('ERROR!')
+
+    finally:
+        time.sleep(0.1)
